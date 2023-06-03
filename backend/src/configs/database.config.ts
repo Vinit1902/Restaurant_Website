58a429ec4415482b4c3579a13c5e0e15562a146e
Vinit Hingmire <90926509+Vinit1902@users.noreply.github.com>
2023-06-03 15:24:13 +0530

    Update database.config.ts

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -1,7 +1,7 @@
 import {connect, ConnectOptions} from 'mongoose';
 
 export const dbConnect = () => {
-    connect(/*process.env.MONGO_URI!*/ "mongodb+srv://vinit:vinit123@atlascluster.9fx37oy.mongodb.net/", {
+    connect(/*process.env.MONGO_URI!*/, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     } as ConnectOptions).then(
